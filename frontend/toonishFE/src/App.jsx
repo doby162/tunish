81b31f6a00b99e58c89da791cb6981e810b94d39
Michael Dorian <doby162@gmail.com>
2025-09-28 12:49:02 -0500

    allow entering one character

--- frontend/toonishFE/src/App.jsx
+++ frontend/toonishFE/src/App.jsx
@@ -6,6 +6,11 @@ import './App.css'
 function App() {
     const [msg, setMsg] = useState('')
     const [messages, setMessages] = useState([])
+    const [shell, setShell] = useState('')
+    const [inner, setInner] = useState('')
+    const [mod, setMod] = useState(false)
+    const vowels = [{pronounce: "A as in glass", display: "æ"}]
+    const consonants = [{pronounce: "B as in baby", display: "b"}]
     let sendMessage = async (msg) => {
         const url = "/messages";
         try {
@@ -42,11 +47,37 @@ function App() {
           })}
         <input type='text' onChange={(e)=>{setMsg(e.target.value)}}/>
         <button onClick={() => sendMessage(msg)}>
-          submit
+          submit <code>(send)</code>
         </button>
           <button onClick={() => getMessages()}>
               refresh
+              <code>(refresh)</code>
           </button>
+
+
+          <div style={{display: "flex"}}>
+
+              <div>
+                  {inner}{shell}{mod? "_" : ""}
+                  <button onClick={()=>{setMod(!mod)}}> <code>mod</code></button>
+                  <code>vowels:</code>
+                  {vowels.map((v)=>{
+                      return <button onClick={()=>setShell(v.display)}>
+                          {v.display}
+                          <code>{v.pronounce}</code>
+                      </button>
+                  })}
+              </div>
+              <div>
+                  <code>consonants:</code>
+                  {consonants.map((v)=>{
+                      return <button onClick={()=>setInner(v.display)}>
+                          {v.display}
+                          <code>{v.pronounce}</code>
+                      </button>
+                  })}
+              </div>
+          </div>
       </div>
     </>
   )
